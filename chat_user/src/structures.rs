5cd19e9ea2ed4b814dc1df5c13bbd03926a38608
Mike <antonovma@outlook.com>
2022-09-03 07:46:34 +0400

    removed structures

--- chat_user/src/structures.rs
+++
@@ -1,9 +0,0 @@
-#![allow(unused)]
-use std::iter::Map;
-use serde::{Deserialize, Serialize};
-
-#[derive(Clone, Debug, Serialize)]
-pub struct UserData {
-    pub name: String,
-    pub message: String,
-}
