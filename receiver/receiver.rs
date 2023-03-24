516da4a1a9eb674a262e8d997b7fc37a93cd07b3
antonovmike <antonovma@outlook.com>
2023-03-24 16:42:49 +0500

    prev messages reversed

--- receiver/receiver.rs
+++ receiver/receiver.rs
@@ -1,10 +1,10 @@
+use crate::receiver_lib::{UserData, UserID};
+use colored::Colorize;
 use std::io::{ErrorKind, Read};
 use std::net::TcpListener;
 use std::sync::mpsc;
 use std::thread;
 use std::time::Duration;
-use crate::receiver_lib::{UserData, UserID};
-use colored::Colorize;
 
 const DATA_SIZE: usize = 96;
 
@@ -16,17 +16,18 @@ pub fn receiver(server: TcpListener) {
     // let connection = sqlite::open(":memory").unwrap();
     // let drop = "DROP TABLE users";
     // connection.execute(drop).unwrap();
-    
+
     let mut clients = vec![];
     let (tx, _rx) = mpsc::channel::<String>();
-    
+
     loop {
         if let Ok((mut socket, addr)) = server.accept() {
             let tx1 = tx.clone();
             clients.push(socket.try_clone().expect("Failed to clone client"));
 
             thread::spawn(move || loop {
-                let connection = sqlite::open(":memory").unwrap();
+                // let connection = sqlite::open(":memory").unwrap();
+                let connection = sqlite::open("chat.db").unwrap();
                 let query = "CREATE TABLE if NOT EXISTS users (name CHARFIELD, message TEXT)";
                 connection.execute(query).unwrap();
 
@@ -37,33 +38,43 @@ pub fn receiver(server: TcpListener) {
                             .into_iter()
                             .take_while(|&x| x != 0)
                             .collect::<Vec<_>>();
-                        let serde_message = String::from_utf8(serde_content).expect("Invalid utf8 message");
+                        let serde_message =
+                            String::from_utf8(serde_content).expect("Invalid utf8 message");
 
-                        let deserialized: UserData = serde_json::from_str(&serde_message).expect("Could not read");
+                        let deserialized: UserData =
+                            serde_json::from_str(&serde_message).expect("Could not read");
 
                         let user_id: UserID = UserID {
                             id: addr.to_string(),
                             data: deserialized,
                         };
-                        
+
                         let username = format!("{}", user_id.data.name);
                         // let usermessage = format!("{}", user_id.data.message);
                         let usermessage = checker(user_id.data.message.clone());
-                        let query = format!("INSERT INTO users VALUES ('{}', '{}')", username, usermessage);
+                        let query = format!(
+                            "INSERT INTO users VALUES ('{}', '{}')",
+                            username, usermessage
+                        );
                         connection.execute(query).unwrap();
 
-                        println!("{} {} {} \n{}", 
+                        println!(
+                            "{} {} {} \n{}",
                             format!("{}", user_id.data.name).bold().yellow(),
                             format!("(ID: {})", user_id.id).bold().purple(),
                             format!("said:").bold().yellow(),
                             format!("\"{}\"", user_id.data.message).italic().on_green()
                         );
 
-                        tx1.send(serde_message).expect("Failed to send message to rx");
-                    },
+                        tx1.send(serde_message)
+                            .expect("Failed to send message to rx");
+                    }
                     Err(ref err) if err.kind() == ErrorKind::WouldBlock => (),
                     Err(_) => {
-                        println!("{}", format!("Closing connection with {}", addr).on_purple());
+                        println!(
+                            "{}",
+                            format!("Closing connection with {}", addr).on_purple()
+                        );
                         break;
                     }
                 }
@@ -87,11 +98,11 @@ fn checker(message: String) -> String {
         let mut replaced = message;
         let mut index = 0;
         for i in escapers {
-            replaced.replace_range(i+index..i+index, "\'");
+            replaced.replace_range(i + index..i + index, "\'");
             index += 1
         }
         replaced
     } else {
-        return message
+        return message;
     }
 }
--- transmitter/transmitter.rs
+++ transmitter/transmitter.rs
@@ -1,52 +1,68 @@
+use crate::transmitter_lib::UserData;
+use colored::Colorize;
+use sqlite::State;
 use std::io::{self, Write};
 use std::net::TcpStream;
 use std::sync::mpsc::{self, TryRecvError};
 use std::thread;
 use std::time::Duration;
-use colored::Colorize;
-use crate::transmitter_lib::UserData;
-use sqlite::State;
 
 const DATA_SIZE: usize = 96;
 
 pub fn transmitter(mut client: TcpStream) {
-    let connection = sqlite::open(":memory").unwrap();
+    let connection = sqlite::open("chat.db").unwrap();
     let query = "CREATE TABLE if NOT EXISTS users (name CHARFIELD, message TEXT)";
     connection.execute(query).unwrap();
-    
+
     println!("Previous messages:");
     let query = "SELECT * FROM users ORDER BY ROWID DESC LIMIT 5";
     let mut statement = connection.prepare(query).unwrap();
+    let mut prev_mess: Vec<String> = vec![];
     while let Ok(State::Row) = statement.next() {
-        println!(
-            "{} {}",
-            format!("{} said:", statement.read::<String, _>("name").unwrap()).bold().yellow(), 
-            format!("{}", statement.read::<String, _>("message").unwrap()).bold().green()
-        )
+        // println!(
+        //     "{} {}",
+        //     format!("{} said:", statement.read::<String, _>("name").unwrap())
+        //         .bold()
+        //         .yellow(),
+        //     format!("{}", statement.read::<String, _>("message").unwrap())
+        //         .bold()
+        //         .green()
+        // )
+        prev_mess.push(format!(
+            "{} said: {}",
+            statement.read::<String, _>("name").unwrap(),
+            statement.read::<String, _>("message").unwrap()
+        ))
+    }
+    // println!("{:?}", prev_mess);
+    prev_mess.reverse();
+    for m in prev_mess {
+        println!("{}", m.bold().green())
     }
 
     let query = "SELECT * FROM users";
     let mut statement = connection.prepare(query).unwrap();
     let mut index = 0;
     while let Ok(State::Row) = statement.next() {
-        index+= 1
+        index += 1
     }
 
     println!("{}", "Enter your name".bold().on_yellow());
-	let mut user_name = String::new();
-	io::stdin().read_line(&mut user_name).expect("Failed to read line");
+    let mut user_name = String::new();
+    io::stdin()
+        .read_line(&mut user_name)
+        .expect("Failed to read line");
     user_name.pop();
 
     let (tx, rx) = mpsc::channel::<String>();
 
     thread::spawn(move || loop {
-
-        let connection = sqlite::open(":memory").unwrap();
+        let connection = sqlite::open("chat.db").unwrap();
         let query = "SELECT * FROM users";
         let mut statement = connection.prepare(query).unwrap();
         let mut index_inner = 0;
         while let Ok(State::Row) = statement.next() {
-            index_inner+= 1
+            index_inner += 1
         }
         if index_inner > index {
             let query = "SELECT * FROM users ORDER BY ROWID DESC LIMIT 1";
@@ -54,13 +70,17 @@ pub fn transmitter(mut client: TcpStream) {
             while let Ok(State::Row) = statement.next() {
                 println!(
                     "{} {}",
-                    format!("{} said:", statement.read::<String, _>("name").unwrap()).bold().yellow(), 
-                    format!("{}", statement.read::<String, _>("message").unwrap()).bold().green()
+                    format!("{} said:", statement.read::<String, _>("name").unwrap())
+                        .bold()
+                        .yellow(),
+                    format!("{}", statement.read::<String, _>("message").unwrap())
+                        .bold()
+                        .green()
                 )
             }
             index += 1
         }
-        
+
         match rx.try_recv() {
             Ok(user_message) => {
                 let user_data = UserData {
@@ -76,21 +96,27 @@ pub fn transmitter(mut client: TcpStream) {
                 let mut buff_serde = serialized.clone();
                 buff_serde.resize(DATA_SIZE, 0);
 
-                client.write_all(&buff_serde).expect("Writing to socket failed");
-            },
+                client
+                    .write_all(&buff_serde)
+                    .expect("Writing to socket failed");
+            }
             Err(TryRecvError::Empty) => (),
-            Err(TryRecvError::Disconnected) => break
+            Err(TryRecvError::Disconnected) => break,
         }
 
         thread::sleep(Duration::from_millis(100));
     });
 
     println!("{}", "Write a message:".bold().on_green());
-    loop{
+    loop {
         let mut buff_message = String::new();
-        io::stdin().read_line(&mut buff_message).expect("Reading from stdin failed");
+        io::stdin()
+            .read_line(&mut buff_message)
+            .expect("Reading from stdin failed");
         let user_message = buff_message.trim().to_string();
-        if user_message == ":quit" || tx.send(user_message).is_err() { break }
+        if user_message == ":quit" || tx.send(user_message).is_err() {
+            break;
+        }
     }
     println!("{}", "Bye".bold().on_blue());
 }
