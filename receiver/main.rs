877033a44ce12b73098348a8a6214d0eae32bca8
antonovmike <antonovma@outlook.com>
2023-05-18 10:02:32 +0500

    tokio

--- receiver/main.rs
+++ receiver/main.rs
@@ -1,14 +1,16 @@
 use std::net::TcpListener;
 
-mod receiver_lib;
 mod receiver;
+mod receiver_lib;
 
 const LOCAL: &str = "127.0.0.1:6000";
 
-fn main() {
+#[tokio::main]
+async fn main() {
     let server = TcpListener::bind(LOCAL)
         .expect("Listener failed to bind");
-    server.set_nonblocking(true)
+    server
+        .set_nonblocking(true)
         .expect("Failed to initialize non-blocking");
     receiver::receiver(server);
 }
