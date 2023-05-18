1202ac8adc3c4346e8239884277adfd4f5aefd30
antonovmike <antonovma@outlook.com>
2023-05-18 10:26:50 +0500

    1 unwrap left - transmitter serde

--- transmitter/main.rs
+++ transmitter/main.rs
@@ -1,4 +1,5 @@
 use std::net::TcpStream;
+use sqlite::Error;
 
 mod transmitter_lib;
 mod transmitter;
@@ -6,10 +7,13 @@ mod transmitter;
 const LOCAL: &str = "127.0.0.1:6000";
 // const STRUCT_SIZE: usize = 96;
 
-fn main() {
+#[tokio::main]
+async fn main() -> Result<(), Error> {
     let client = TcpStream::connect(LOCAL)
         .expect("Stream failed to connect");
     client.set_nonblocking(true)
         .expect("Failed to initiate non-blocking");
-        transmitter::transmitter(client);
+        transmitter::transmitter(client)?;
+        
+        Ok(())
 }
--- transmitter/transmitter.rs
+++ transmitter/transmitter.rs
@@ -6,21 +6,22 @@ use std::net::TcpStream;
 use std::sync::mpsc::{self, TryRecvError};
 use std::thread;
 use std::time::Duration;
+use sqlite::Error;
 
 const DATA_SIZE: usize = 96;
 
-pub fn transmitter(mut client: TcpStream) {
-    let connection = sqlite::open("chat.db").unwrap();
+pub fn transmitter(mut client: TcpStream) -> Result<(), Error> {
+    let connection = sqlite::open("chat.db")?;
     let query = "CREATE TABLE if NOT EXISTS users (name CHARFIELD, message TEXT)";
-    connection.execute(query).unwrap();
+    connection.execute(query)?;
 
     println!("Previous messages:");
     let query = "SELECT * FROM users ORDER BY ROWID DESC LIMIT 5";
-    let mut statement = connection.prepare(query).unwrap();
+    let mut statement = connection.prepare(query)?;
     let mut prev_mess: Vec<(String, String)> = vec![];
     while let Ok(State::Row) = statement.next() {
-        prev_mess.push((statement.read::<String, _>("name").unwrap(),
-            statement.read::<String, _>("message").unwrap())
+        prev_mess.push((statement.read::<String, _>("name")?,
+            statement.read::<String, _>("message")?)
         );
     }
     prev_mess.reverse();
@@ -31,7 +32,7 @@ pub fn transmitter(mut client: TcpStream) {
         }
 
     let query = "SELECT * FROM users";
-    let mut statement = connection.prepare(query).unwrap();
+    let mut statement = connection.prepare(query)?;
     let mut index = 0;
     while let Ok(State::Row) = statement.next() {
         index += 1
@@ -46,55 +47,59 @@ pub fn transmitter(mut client: TcpStream) {
 
     let (tx, rx) = mpsc::channel::<String>();
 
-    thread::spawn(move || loop {
-        let connection = sqlite::open("chat.db").unwrap();
-        let query = "SELECT * FROM users";
-        let mut statement = connection.prepare(query).unwrap();
-        let mut index_inner = 0;
-        while let Ok(State::Row) = statement.next() {
-            index_inner += 1
-        }
-        if index_inner > index {
-            let query = "SELECT * FROM users ORDER BY ROWID DESC LIMIT 1";
-            let mut statement = connection.prepare(query).unwrap();
+    thread::spawn(move || -> Result<(), Error> {
+        loop {
+            let connection = sqlite::open("chat.db")?;
+            let query = "SELECT * FROM users";
+            let mut statement = connection.prepare(query)?;
+            let mut index_inner = 0;
             while let Ok(State::Row) = statement.next() {
-                println!(
-                    "{} {}",
-                    format!("{} said:", statement.read::<String, _>("name").unwrap())
-                        .bold()
-                        .yellow(),
-                    format!("{}", statement.read::<String, _>("message").unwrap())
-                        .bold()
-                        .green()
-                )
+                index_inner += 1
             }
-            index += 1
+            if index_inner > index {
+                let query = "SELECT * FROM users ORDER BY ROWID DESC LIMIT 1";
+                let mut statement = connection.prepare(query)?;
+                while let Ok(State::Row) = statement.next() {
+                    println!(
+                        "{} {}",
+                        format!("{} said:", statement.read::<String, _>("name")?)
+                            .bold()
+                            .yellow(),
+                        format!("{}", statement.read::<String, _>("message")?)
+                            .bold()
+                            .green()
+                    )
+                }
+                index += 1
+            }
+
+            match rx.try_recv() {
+                Ok(user_message) => {
+                    let user_data = UserData {
+                        name: user_name.clone(),
+                        message: user_message.clone(),
+                    };
+
+                    let serialized = serde_json::to_string(&user_data)
+                        .unwrap()
+                        .clone()
+                        .into_bytes();
+
+                    let mut buff_serde = serialized.clone();
+                    buff_serde.resize(DATA_SIZE, 0);
+
+                    client
+                        .write_all(&buff_serde)
+                        .expect("Writing to socket failed");
+                }
+                Err(TryRecvError::Empty) => (),
+                Err(TryRecvError::Disconnected) => break,
+            }
+
+            thread::sleep(Duration::from_millis(100));
         }
 
-        match rx.try_recv() {
-            Ok(user_message) => {
-                let user_data = UserData {
-                    name: user_name.clone(),
-                    message: user_message.clone(),
-                };
-
-                let serialized = serde_json::to_string(&user_data)
-                    .unwrap()
-                    .clone()
-                    .into_bytes();
-
-                let mut buff_serde = serialized.clone();
-                buff_serde.resize(DATA_SIZE, 0);
-
-                client
-                    .write_all(&buff_serde)
-                    .expect("Writing to socket failed");
-            }
-            Err(TryRecvError::Empty) => (),
-            Err(TryRecvError::Disconnected) => break,
-        }
-
-        thread::sleep(Duration::from_millis(100));
+        Ok(())
     });
 
     println!("{}", "Write a message:".bold().on_green());
@@ -109,4 +114,6 @@ pub fn transmitter(mut client: TcpStream) {
         }
     }
     println!("{}", "Bye".bold().on_blue());
+
+    Ok(())
 }
