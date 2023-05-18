17227ec766eb6b974c98f2879feea03b5111cfb5
antonovmike <antonovma@outlook.com>
2023-05-18 10:14:13 +0500

    thiserror

--- receiver/receiver.rs
+++ receiver/receiver.rs
@@ -8,15 +8,20 @@ use std::time::Duration;
 
 const DATA_SIZE: usize = 96;
 
+#[derive(Debug, thiserror::Error)]
+pub enum Error {
+    #[error("failed to open file: {0}")]
+    IoError(#[from] std::io::Error),
+
+    #[error("failed to open file: {0}")]
+    SqliteError(#[from] sqlite::Error),
+}
+
 fn sleep() {
     thread::sleep(Duration::from_millis(100));
 }
 
-pub fn receiver(server: TcpListener) {
-    // let connection = sqlite::open(":memory").unwrap();
-    // let drop = "DROP TABLE users";
-    // connection.execute(drop).unwrap();
-
+pub fn receiver(server: TcpListener) -> Result<(), Error> {
     let mut clients = vec![];
     let (tx, _rx) = mpsc::channel::<String>();
 
@@ -25,60 +30,62 @@ pub fn receiver(server: TcpListener) {
             let tx1 = tx.clone();
             clients.push(socket.try_clone().expect("Failed to clone client"));
 
-            thread::spawn(move || loop {
-                // let connection = sqlite::open(":memory").unwrap();
-                let connection = sqlite::open("chat.db").unwrap();
-                let query = "CREATE TABLE if NOT EXISTS users (name CHARFIELD, message TEXT)";
-                connection.execute(query).unwrap();
+            thread::spawn(move || -> Result<(), Error> {
+                loop {
+                    let connection = sqlite::open("chat.db")?;
+                    let query = "CREATE TABLE if NOT EXISTS users (name CHARFIELD, message TEXT)";
+                    connection.execute(query)?;
 
-                let mut buff_serde = vec![0; DATA_SIZE];
-                match socket.read_exact(&mut buff_serde) {
-                    Ok(_) => {
-                        let serde_content = buff_serde
-                            .into_iter()
-                            .take_while(|&x| x != 0)
-                            .collect::<Vec<_>>();
-                        let serde_message =
-                            String::from_utf8(serde_content).expect("Invalid utf8 message");
+                    let mut buff_serde = vec![0; DATA_SIZE];
+                    match socket.read_exact(&mut buff_serde) {
+                        Ok(_) => {
+                            let serde_content = buff_serde
+                                .into_iter()
+                                .take_while(|&x| x != 0)
+                                .collect::<Vec<_>>();
+                            let serde_message =
+                                String::from_utf8(serde_content).expect("Invalid utf8 message");
 
-                        let deserialized: UserData =
-                            serde_json::from_str(&serde_message).expect("Could not read");
+                            let deserialized: UserData =
+                                serde_json::from_str(&serde_message).expect("Could not read");
 
-                        let user_id: UserID = UserID {
-                            id: addr.to_string(),
-                            data: deserialized,
-                        };
+                            let user_id: UserID = UserID {
+                                id: addr.to_string(),
+                                data: deserialized,
+                            };
 
-                        let username = format!("{}", user_id.data.name);
-                        // let usermessage = format!("{}", user_id.data.message);
-                        let usermessage = checker(user_id.data.message.clone());
-                        let query = format!(
-                            "INSERT INTO users VALUES ('{}', '{}')",
-                            username, usermessage
-                        );
-                        connection.execute(query).unwrap();
+                            let username = format!("{}", user_id.data.name);
+                            // let usermessage = format!("{}", user_id.data.message);
+                            let usermessage = checker(user_id.data.message.clone());
+                            let query = format!(
+                                "INSERT INTO users VALUES ('{}', '{}')",
+                                username, usermessage
+                            );
+                            connection.execute(query).unwrap();
 
-                        println!(
-                            "{} {} {} \n{}",
-                            format!("{}", user_id.data.name).bold().yellow(),
-                            format!("(ID: {})", user_id.id).bold().purple(),
-                            format!("said:").bold().yellow(),
-                            format!("\"{}\"", user_id.data.message).italic().on_green()
-                        );
+                            println!(
+                                "{} {} {} \n{}",
+                                format!("{}", user_id.data.name).bold().yellow(),
+                                format!("(ID: {})", user_id.id).bold().purple(),
+                                format!("said:").bold().yellow(),
+                                format!("\"{}\"", user_id.data.message).italic().on_green()
+                            );
 
-                        tx1.send(serde_message)
-                            .expect("Failed to send message to rx");
-                    }
-                    Err(ref err) if err.kind() == ErrorKind::WouldBlock => (),
-                    Err(_) => {
-                        println!(
-                            "{}",
-                            format!("Closing connection with {}", addr).on_purple()
-                        );
-                        break;
+                            tx1.send(serde_message)
+                                .expect("Failed to send message to rx");
+                        }
+                        Err(ref err) if err.kind() == ErrorKind::WouldBlock => (),
+                        Err(_) => {
+                            println!(
+                                "{}",
+                                format!("Closing connection with {}", addr).on_purple()
+                            );
+                            break;
+                        }
                     }
                 }
                 sleep();
+                Ok(())
             });
         }
 
