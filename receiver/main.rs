79468a5bf2e0d8d7fca2e845e1e3216623fa9fe2
antonovmike <antonovma@outlook.com>
2023-06-18 12:54:39 +0500

    add constant

--- receiver/main.rs
+++ receiver/main.rs
@@ -1,15 +1,14 @@
-use std::net::TcpListener;
 use sqlite::Error;
+use std::net::TcpListener;
 
 mod receiver;
 mod receiver_lib;
 
-const LOCAL: &str = "127.0.0.1:6000";
+const LOCAL: &str = "127.0.0.1:8080";
 
 #[tokio::main]
 async fn main() -> Result<(), Error> {
-    let server = TcpListener::bind(LOCAL)
-        .expect("Listener failed to bind");
+    let server = TcpListener::bind(LOCAL).expect("Listener failed to bind");
     server
         .set_nonblocking(true)
         .expect("Failed to initialize non-blocking");
